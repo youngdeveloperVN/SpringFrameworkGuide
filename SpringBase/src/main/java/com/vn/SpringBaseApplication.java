3860caace0d513ae8ed403d8c9dd282168a30d6d
Huy Pham <huypn6@fpt.edu.vn>
2021-04-13 22:31:52 +0700

    add code base

---
+++ SpringBase/src/main/java/com/vn/SpringBaseApplication.java
@@ -0,0 +1,13 @@
+package com.vn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBaseApplication.class, args);
+	}
+
+}
